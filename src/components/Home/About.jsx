d3ebb3ac7b21578c75810bda17a5b05c37b4a2f0
BASIM-JAVED <basim.63297@iqra.edu.pk>
2023-12-07 03:10:42 +0500

    portfolio-spidy

---
+++ src/components/Home/About.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import LaptopImg from "../../assets/home-main.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+
+const About = () => {
+    return (
+        <Container fluid className="home-about-section" id="about">
+          <Container>
+            <Row>
+              <Col md={8} className="home-about-description">
+                <h1 style={{ fontSize: "2.6em" }}>
+                  LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+                </h1>
+                <p className="home-about-body">
+                 Hi, my name is <span className="yellow">BASIM JAVED </span>
+                 and I'm from <span className="yellow"> KARACHI, PAKISTAN.</span>
+                <br />
+                <br />
+                I recently graduated with a Bachelor's degree in Computer Science in 2021.
+                <br />
+                <br />
+                  As a
+                  <b className="yellow"> MERN-Stack </b>developer,  
+                  I enjoy tackling new challenges and continuously expanding my skillset.
+                  <br />
+                  <br />I am proficient in
+                    <b className="yellow"> Javascript, </b>
+                    as well as have knowledge in programming languages such as C, Java, Python, PHP
+                  <b className="yellow"> </b>
+                  <br />
+                  <br />
+                 
+                  
+                </p>
+              </Col>
+              <Col md={4} className="myAvtar">
+                <Tilt>
+                  <img src={LaptopImg} className="img-fluid" alt="avatar" />
+                </Tilt>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12} className="home-about-social">
+                <h1>FIND ME ON</h1>
+                <p>
+                Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+                </p>
+                <ul className="home-about-social-links">
+                  <li className="social-icons">
+                    <a
+                      href="https://github.com/BASIM-JAVED"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                      aria-label="github"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  
+                  <li className="social-icons">
+                    <a
+                      href="https://www.linkedin.com/in/basim-javed-494684276/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                      aria-label="linkedin"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </Container>
+      );
+    
+}
+
+export default About
